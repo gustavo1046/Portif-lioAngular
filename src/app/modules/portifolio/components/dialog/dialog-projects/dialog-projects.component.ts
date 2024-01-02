01775e997aefa6db7c582c566dcf5695779fa6f2
Gustavo <gustavo1046@hotmail.com>
2024-01-02 10:45:07 -0300

    finalizado o projeto

--- src/app/modules/portifolio/components/dialog/dialog-projects/dialog-projects.component.ts
+++ src/app/modules/portifolio/components/dialog/dialog-projects/dialog-projects.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, signal } from '@angular/core';
+import { IProjects } from '../../../../Interface/IProjects';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog-projects',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './dialog-projects.component.html',
   styleUrl: './dialog-projects.component.scss'
 })
-export class DialogProjectsComponent {
+export class DialogProjectsComponent implements OnInit{
+  
+  constructor(private _dialogRef: MatDialogRef<DialogProjectsComponent>, @Inject(MAT_DIALOG_DATA) private _data : IProjects){
+    
+  }
 
+  public getData= signal<IProjects | null>(null);
+
+  ngOnInit(): void {
+      this.getData.set(this._data)
+  }
+
+  public closeModal(){
+    return this._dialogRef.close();
+  }
 }
